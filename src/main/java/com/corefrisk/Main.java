ded6c9f39475a8204eea42e5a3b125050825e5f3
CoreFrisk <martakova_aa@list.ru>
2024-12-05 18:06:32 +0300

    right answer day1 task2

--- src/main/java/com/corefrisk/Main.java
+++ src/main/java/com/corefrisk/Main.java
@@ -1,17 +1,6 @@
 package com.corefrisk;
 
-import com.corefrisk.day1.DistanceCalculator;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        Integer l = DistanceCalculator.calculateTotalDistance("/input.txt");
-        System.out.println(l);
     }
 }
--- src/main/java/com/corefrisk/day1/DistanceCalculator.java
+++ src/main/java/com/corefrisk/day1/DistanceCalculator.java
@@ -9,41 +9,72 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 
 public class DistanceCalculator {
 
     public static Integer calculateTotalDistance(String input) {
         List<Integer>[] numbers = parseInputFromFileToArrays(input);
+        blablabla(numbers[0], numbers[1]);
         return calculateSumOfDifferences(numbers[0], numbers[1]);
     }
 
-    private static List<Integer>[] parseInputFromFileToArrays(String input) {
+    public static List<Integer>[] parseInputFromFileToArrays(String input) {
+
         List<Integer> left = new ArrayList<>(1000);
         List<Integer> right = new ArrayList<>(1000);
+
         try (InputStream inputStream = Main.class.getResourceAsStream(input);
              BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+
             String currentLine;
+
             while (((currentLine = br.readLine()) != null)) {
+
                 String[] parts = currentLine.trim().split("\\s+");
                 left.add(Integer.valueOf(parts[0]));
                 right.add(Integer.valueOf(parts[1]));
+
             }
+
             left.sort(Integer::compareTo);
             right.sort(Integer::compareTo);
             return new List[]{left, right};
 
         } catch (IOException e) {
+
             throw new RuntimeException(e);
+
         }
+
     }
-    private static Integer calculateSumOfDifferences(List<Integer> left, List<Integer> right) {
+
+    public static void blablabla(List<Integer> left, List<Integer> right) {
+
+        long totalResult = 0;
+
+        for (Integer i : left) {
+
+            long countInRight = right.stream().filter(number -> number.equals(i)).count();
+            long result = i * countInRight;
+            totalResult += result;
+//            System.out.println("Результат для " + i + ": " + result);
+
+        }
+
+//        System.out.println("Общий результат: " + totalResult);
+
+    }
+
+    public static Integer calculateSumOfDifferences(List<Integer> left, List<Integer> right) {
+
         return IntStream.range(0, left.size())
                 .map(i -> Math.abs(left.get(i) - right.get(i)))
                 .sum();
+
     }
 
-    private static List<Integer>[] parseInputToParts(String input) {
+    public static List<Integer>[] parseInputToParts(String input) {
+
         String[] parts = input.trim().split("\\s+");
         List<Integer> left = parts[0].chars()
                 .map(Character::getNumericValue)
@@ -56,6 +87,7 @@ public class DistanceCalculator {
                 .boxed()
                 .toList();
         return new List[]{left, right};
+
     }
 
 
