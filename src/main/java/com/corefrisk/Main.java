3bf41a08edcb827224c7893791af0c3eca15ae76
CoreFrisk <martakova_aa@list.ru>
2024-12-05 14:19:54 +0300

    1

--- src/main/java/com/corefrisk/Main.java
+++ src/main/java/com/corefrisk/Main.java
@@ -1,7 +1,30 @@
 package com.corefrisk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+//        File myFile = new File("word.txt");
+//        System.out.println("Attempting to read from file in: "+myFile.getCanonicalPath());
+//
+//        Scanner input = new Scanner(myFile);
+//        String in = "";
+//        in = input.nextLine();
+        String s = null;
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\CoreFrisk\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\input.txt"))) {
+            String currentLine;
+            while ((currentLine = br.readLine()) != null) {
+                System.out.println(currentLine);
+                s = currentLine;
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/corefrisk/day1/ReaderAndComparatorOfTwoPairsOfNumbers.java
+++ src/main/java/com/corefrisk/day1/ReaderAndComparatorOfTwoPairsOfNumbers.java
@@ -1,4 +1,26 @@
 package com.corefrisk.day1;
 
-public class  {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReaderAndComparatorOfTwoPairsOfNumbers {
+
+    public Long howFarItIs() throws FileNotFoundException {
+        String s = null;
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+            String currentLine;
+            while ((currentLine = br.readLine()) != null) {
+                System.out.println(currentLine);
+                s = currentLine;
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+         return Long.parseLong(s);
+    }
 }
